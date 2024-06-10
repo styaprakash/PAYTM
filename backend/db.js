12cfb71ea963495a6e41bb6d4f39c7bd3413dcad
Satya prakash mishra <satyaprakashmishra23.12@gmail.com>
2024-06-10 20:07:18 +0530

    Create a new router for accounts and manage transactions for mongodb

--- backend/db.js
+++ backend/db.js
@@ -31,8 +31,21 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to User model
+      ref: 'User',
+      required: true
+  },
+  balance: {
+      type: Number,
+      required: true
+  }
+});
+
+const Account = mongoose.model("User",accountSchema);
 const User = mongoose.model("User",userSchema);
 
 model.exports = {
-    User
+    User, Account
 }
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors);
-app.use(express.json());
 const mainRouter = require("./routes/index");
 
 const app = express();
+
+app.use(cors);
+app.use(express.json());
+
 
 app.use("/api/v1", mainRouter);
 app.listen(3000);
