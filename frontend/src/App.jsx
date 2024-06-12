e3daea4c89a72bb5da3271f39ffa64cab5e65d0f
Satya prakash mishra <satyaprakashmishra23.12@gmail.com>
2024-06-12 17:33:34 +0530

    Created routes for dedicated tasks and added various components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowseRouter>
+        <Routes>
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/SendMoney" element={<SendMoney />} />
+        </Routes>
+      </BrowseRouter>  
+    </>
   )
 }
 
---
+++ frontend/src/pages/SendMoney.jsx
@@ -0,0 +1,8 @@
+
+export const SendMoney = () => {
+    return (
+        <div>
+            send money page
+        </div>
+    )
+}
